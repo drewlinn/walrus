0b50171c5e9a27330166e10b11b58485bd015716
Andrew Dalton and Andrea Faciane <andrea@>
2017-02-06 20:56:41 -0800

    begin adding dynamic elements that will allow the user to change the theme.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function()  {
     $(".initially-showing").toggle();
     $(".initially-hidden").toggle();
   });
+  $("button#original").click(function() {
+    $("body").removeClass();
+  });
+  $("button#alternate").click(function() {
+    $("body").addClass("alternate-theme");
+  });
 });
